6b808b9d8071022d9c3abb59e34e3fe12d89c3eb
Nicolas <feriolinicolas@gmail.com>
2024-04-10 03:09:10 -0300

    punto 1 tp 1 terminado (menos toString)

--- tp1/MySimpleLinkedList.java
+++ tp1/MySimpleLinkedList.java
@@ -3,35 +3,44 @@ package ProgramacionIII.tp1;
 public class MySimpleLinkedList<T> {
 	
 	private Node<T> first;
+	private int size = 0;
 	
 	public MySimpleLinkedList() {
 		this.first = null;
 	}
 	
-	public void insertFront(T info) {
+	public void insertFront(T info) { // O(1)
 		Node<T> tmp = new Node<T>(info,null);
 		tmp.setNext(this.first);
 		this.first = tmp;
+		this.size++;
 	}
 	
-	public T extractFront() {		
-		// TODO
-		return null;
+	public T extractFront() { // O(1)
+		return this.getFirst().getInfo();
 	}
 
-	public boolean isEmpty() {
-		// TODO
-		return false;
+	public boolean isEmpty() { // O(1)
+		return this.first == null;
 	}
 	
-	public T get(int index) {
-		// TODO
-		return null;
+	public T get(int index) { // O(n) (ya que tendrá que recorrer TODA la lista para llegar al indice deseado)
+		Node<T> actual = this.first;
+		
+		if(index < 0) {
+			throw new IndexOutOfBoundsException("Indice no valido");
+		}else if(index >= this.size()){
+			throw new IndexOutOfBoundsException("El indice supera el tamaño de la lista");
+		}
+
+		for(int i = 0; i < index; i++){ 
+			actual = actual.getNext();
+		}
+		return actual.getInfo();
 	}
 	
-	public int size() {
-		// TODO
-		return 0;
+	public int size() { // O(1)
+		return this.size;
 	}
 	
 	@Override
